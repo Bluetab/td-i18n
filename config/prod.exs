a6e7538492329c2b93e0f6fc1d4646290ca30ffa
Tom Crossland <tom.crossland@bluetab.net>
2022-11-14 19:03:08 +0100

    refactor: /app/messages.json for prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,12 @@
 import Config
 
 config :td_i18n, TdI18nWeb.Endpoint, server: true
+
+config :td_i18n, TdI18n.Scheduler,
+  jobs: [
+    [
+      schedule: "@reboot",
+      task: {TdI18n.Locales, :load_from_file!, ["/app/messages.json"]},
+      run_strategy: Quantum.RunStrategy.Local
+    ]
+  ]
