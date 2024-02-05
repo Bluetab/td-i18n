40a24b17a03e72df25b9ceebf8d62990bac796dd
alejandro.oliva <alejandro.oliva@bluetab.net>
2024-02-05 16:38:59 +0000

    TD-6374 Refactor locales to include lang manager

--- test/td_i18n/locales/locale_test.exs
+++ test/td_i18n/locales/locale_test.exs
@@ -5,25 +5,38 @@ defmodule TdI18n.Locales.LocaleTest do
 
   describe "changeset/2" do
     test "validates fields valid values" do
-      assert %{errors: []} = Locale.changeset(%Locale{}, %{lang: "td"})
+      assert %{errors: []} =
+               Locale.changeset(%Locale{}, %{lang: "td", name: "Truedat", local_name: "Truedis"})
     end
 
     test "validates required fields" do
       assert %{errors: errors} = Locale.changeset(%Locale{}, %{})
 
       assert {_, [validation: :required]} = errors[:lang]
+      assert {_, [validation: :required]} = errors[:name]
+      assert {_, [validation: :required]} = errors[:local_name]
     end
 
     test "validates field types" do
       assert %{errors: errors} =
                Locale.changeset(
                  %Locale{},
-                 %{lang: 1, is_required: "lorem", is_default: "ipsum"}
+                 %{
+                   lang: 1,
+                   is_required: "lorem",
+                   is_default: "ipsum",
+                   is_enabled: "dolor",
+                   name: 1,
+                   local_name: 1
+                 }
                )
 
       assert {_, [{:type, :string}, {:validation, :cast}]} = errors[:lang]
+      assert {_, [{:type, :string}, {:validation, :cast}]} = errors[:name]
+      assert {_, [{:type, :string}, {:validation, :cast}]} = errors[:local_name]
       assert {_, [{:type, :boolean}, {:validation, :cast}]} = errors[:is_required]
       assert {_, [{:type, :boolean}, {:validation, :cast}]} = errors[:is_default]
+      assert {_, [{:type, :boolean}, {:validation, :cast}]} = errors[:is_enabled]
     end
   end
 end
