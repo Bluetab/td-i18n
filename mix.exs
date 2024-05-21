0715ab3645baf71a0fa7bae800a1c75a6ff45472
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-05-21 10:34:58 +0200

    chore: TD-6582 version 6.6.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdI18n.MixProject do
       app: :td_i18n,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "6.5.0-local"
+          nil -> "6.6.0-local"
           v -> v
         end,
       elixir: "~> 1.13",
